83dc9fed7532a1699d89094ae225fb1a2782e66a
Martin Račko <info@qubase.sk>
2014-01-27 22:13:45 +0100

    suite - forklift.de, beta testing, encoding issues still present

--- src/qubase/suite/Forklift.java
+++ src/qubase/suite/Forklift.java
@@ -2,6 +2,8 @@ package qubase.suite;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,66 +30,66 @@ public class Forklift extends Crawler {
 		boolean inSelectBox = false;
 		String optionRegexp = "<option\\s*value=\"([0-9]+),(0)\"\\s*style=\"font-weight:\\s*bold;\">([a-zA-Z0-9]+)</option>";
 		String listLinkReplace = "http://www.forklift.de/de/endkunde/staplersuche3.php?Bauart=$1,$2&sonderbit=0&reifen=*&Fabrikat=alle&antriebsart=*&masttypid=alle&tkvon=0&tkbis=100000&bhvon=0&bhbis=9000&hhvon=0&hhbis=30000&fhvon=0&fhbis=4000&baujahr=0&bjbis=2014&preisvon=0&preisbis=1000000&landid=*&entfernung=0&hatbild=0&numbers=50&page=1";
-//		for (String lineIn : lines) {
-//			String line = lineIn.trim();
-//			if (line.matches("<select\\s*name=\"Bauart\"\\s*id=\"Bauart\".*?>")) {
-//				inSelectBox = true;
-//				continue;//no need to perform the other regular expressions
-//			}
-//			
-//			if (line.matches(optionRegexp) && inSelectBox) {
-//				try {
-//					addToSiteMap(new SiteMapLocation(new URL(line.replaceAll(optionRegexp, listLinkReplace)), line.replaceAll(optionRegexp, "$3")));
-//				} catch (MalformedURLException e) {
-//					logger.severe("Failed to parse URL: " + line.replaceAll(optionRegexp, listLinkReplace));
-//				}
-//			}
-//			
-//			if (inSelectBox && line.matches(".*?</select>")) {
-//				inSelectBox = false;
-//				break;
-//			}
-//		}
+		for (String lineIn : lines) {
+			String line = lineIn.trim();
+			if (line.matches("<select\\s*name=\"Bauart\"\\s*id=\"Bauart\".*?>")) {
+				inSelectBox = true;
+				continue;//no need to perform the other regular expressions
+			}
+			
+			if (line.matches(optionRegexp) && inSelectBox) {
+				try {
+					addToSiteMap(new SiteMapLocation(new URL(line.replaceAll(optionRegexp, listLinkReplace)), line.replaceAll(optionRegexp, "$3")));
+				} catch (MalformedURLException e) {
+					logger.severe("Failed to parse URL: " + line.replaceAll(optionRegexp, listLinkReplace));
+				}
+			}
+			
+			if (inSelectBox && line.matches(".*?</select>")) {
+				inSelectBox = false;
+				break;
+			}
+		}
 		
-//		//parse cleaning machines
-//		input = null;
-//		try {
-//			input = loadCustomPage(new URL("http://www.forklift.de/de/indexrt.php"));
-//		} catch (MalformedURLException e) {
-//			logger.severe("Failed to parse URL: http://www.forklift.de/de/indexrt.php");
-//		}
-//		
-//		if (input != null) {
-//			lines = input.split("\\r?\\n");
-//			for (String lineIn : lines) {
-//				String line = lineIn.trim();
-//				if (line.matches("<select\\s*name=\"Bauart\"\\s*id=\"Bauart\".*?>")) {
-//					inSelectBox = true;
-//					continue;//no need to perform the other regular expressions
-//				}
-//				
-//				if (line.matches("<option.*") && inSelectBox) {
-//					Pattern pattern = Pattern.compile("<option\\s*value=\"([0-9]+)\">(.*?)</option>");
-//			        Matcher  matcher = pattern.matcher(line);
-//			        
-//			        while (matcher.find()) {
-//			        	try {
-//							addToSiteMap(new SiteMapLocation(new URL("http://www.forklift.de/de/endkunde/kehr/suche.php?plz=&entfernung=0&Bauart=" 
-//										+ matcher.group(1) 
-//										+ "&Fabrikat=alle&antriebsartid=alle&kbvon=0&kbbis=4000&preisvon=0&preisbis=1000000&landid=*&numbers=50&page=1")
-//								, matcher.group(2)));
-//						} catch (MalformedURLException e) {
-//							logger.severe("Failed to parse URL: " + siteMapUrl + matcher.group(1));
-//						}
-//			        }
-//				}
-//				
-//				if (line.matches(".*?</select>") && inSelectBox) {
-//					inSelectBox = false;
-//					break;
-//				}
-//			}
-//		}
+		//parse cleaning machines
+		input = null;
+		try {
+			input = loadCustomPage(new URL("http://www.forklift.de/de/indexrt.php"));
+		} catch (MalformedURLException e) {
+			logger.severe("Failed to parse URL: http://www.forklift.de/de/indexrt.php");
+		}
+		
+		if (input != null) {
+			lines = input.split("\\r?\\n");
+			for (String lineIn : lines) {
+				String line = lineIn.trim();
+				if (line.matches("<select\\s*name=\"Bauart\"\\s*id=\"Bauart\".*?>")) {
+					inSelectBox = true;
+					continue;//no need to perform the other regular expressions
+				}
+				
+				if (line.matches("<option.*") && inSelectBox) {
+					Pattern pattern = Pattern.compile("<option\\s*value=\"([0-9]+)\">(.*?)</option>");
+			        Matcher  matcher = pattern.matcher(line);
+			        
+			        while (matcher.find()) {
+			        	try {
+							addToSiteMap(new SiteMapLocation(new URL("http://www.forklift.de/de/endkunde/kehr/suche.php?plz=&entfernung=0&Bauart=" 
+										+ matcher.group(1) 
+										+ "&Fabrikat=alle&antriebsartid=alle&kbvon=0&kbbis=4000&preisvon=0&preisbis=1000000&landid=*&numbers=50&page=1")
+								, matcher.group(2)));
+						} catch (MalformedURLException e) {
+							logger.severe("Failed to parse URL: " + siteMapUrl + matcher.group(1));
+						}
+			        }
+				}
+				
+				if (line.matches(".*?</select>") && inSelectBox) {
+					inSelectBox = false;
+					break;
+				}
+			}
+		}
 		
 		//parse platforms
 		//same code as cleaning machines, different URLs, could be a function, fuck it
@@ -177,13 +179,23 @@ public class Forklift extends Crawler {
 		
 		String potentialBrand = null;
 		String potentialCategory = null;
+		String potentialModel = null;
+		String brand = null;
+		String category = null;
+		String model = null;
 		
 		String regexPotentialBrand = ".*?<h1>(.*?)</h1>$";
 		String regexPotentialCategory = ".*?<h2>(.*?)</h2>$";
+		String regexAddress = "<div\\s*class=\"adress\">(.*?)</div>";
 		
 		String[] lines = input.split("\\r?\\n");
 		for (int i = 0; i < lines.length; i++) {
 			String line = lines[i].trim();
+			
+			if (line.matches("</div><!--\\s*#detailView\\s*-->") && inTheMainDiv) {
+				inTheMainDiv = false;
+				break;
+			}
 			
 			if (line.matches("<div\\s*class=\"main_content_bg\">")) {
 				inTheMainDiv = true;
@@ -197,15 +209,112 @@ public class Forklift extends Crawler {
 				potentialCategory = line.replaceAll(regexPotentialCategory, "$1");
 			}
 			
-			if (line.matches("</div><!--\\s*#detailView\\s*-->") && inTheMainDiv) {
-				inTheMainDiv = false;
-				break;
+			if (inTheMainDiv && line.matches("<span\\s*class=\"label\">Fabrikat:</span>")) {
+				i++;
+				if (lines.length > i) {
+					line = lines[i].trim();
+					brand = line.replaceAll("<span\\s*class=\"value\">(.*?)</span>", "$1");
+				}
+			}
+			
+			if (inTheMainDiv && line.matches("<span\\s*class=\"label\">Baujahr:</span>")) {
+				i++;
+				if (lines.length > i) {
+					line = lines[i].trim();
+					currentListing.setYear(line.replaceAll("<span\\s*class=\"value\">([0-9]+)</span>", "$1"));
+				}
+			}
+			
+			if (inTheMainDiv && line.matches("<span\\s*class=\"label\">Betriebsstunden:</span>")) {
+				i++;
+				if (lines.length > i) {
+					line = lines[i].trim();
+					currentListing.setCounter(line.replaceAll("<span\\s*class=\"value\">([0-9\\., ']+)</span>", "$1"));
+					if (currentListing.getCounter().equals("0")) {
+						currentListing.setCounter(null);
+					}
+				}
+			}
+			
+			if (inTheMainDiv && line.matches("<span\\s*class=\"label\">Fahrgestellnummer:</span>")) {
+				i++;
+				if (lines.length > i) {
+					line = lines[i].trim();
+					currentListing.setSerial(line.replaceAll("<span\\s*class=\"value\">(.*?)</span>", "$1"));
+				}
+			}
+			
+			if (inTheMainDiv && line.matches("<span\\s*class=\"label\">Bauart:</span>")) {
+				i++;
+				if (lines.length > i) {
+					line = lines[i].trim();
+					category = line.replaceAll("<span\\s*class=\"value\">(.*?)</span>", "$1");
+				}
+			}
+			
+			if (inTheMainDiv && line.matches("<span\\s*class=\"label\">Typ:</span>")) {
+				i++;
+				if (lines.length > i) {
+					line = lines[i].trim();
+					model = line.replaceAll("<span\\s*class=\"value\">(.*?)</span>", "$1");
+				}
+			}
+			
+			if (inTheMainDiv && potentialModel == null && line.matches("<div\\s*class=\"boxInnerHeadline\">")) {
+				i++;
+				if (lines.length > i) {
+					line = lines[i].trim();
+					potentialModel = line.replaceAll("(.*?)</div>", "$1").trim();
+				}
+			}
+			
+			if (inTheMainDiv && line.matches("<span\\s*class=\"labelHighlight\">Verkaufspreis.*?</span>")) {
+				i++;
+				if (lines.length > i) {
+					line = lines[i].trim();
+					String priceCurrency = line.replaceAll("<span\\s*class=\"valueHighlight\">(.*?)</span>", "$1");
+					if (priceCurrency.matches(".*[0-9].*")) {
+						currentListing.setCurrency(priceCurrency.replaceAll("[^A-Z]", ""));
+						currentListing.setPrice(priceCurrency.replaceFirst(currentListing.getCurrency(), "").trim());
+					}
+				}
+			}
+			
+			if (inTheMainDiv && line.matches(regexAddress)) {
+				String address = line.replaceAll(regexAddress, "$1");
+				String[] addressCrumbs = address.split("<br>", -1);
+				String country = addressCrumbs[addressCrumbs.length - 1].trim();
+				String zipRegion = addressCrumbs[addressCrumbs.length - 2].trim();
+				String zip = zipRegion.replaceAll("^.*?([-A-Z0-9]{2,3})(\\s)?([-A-Z0-9]{2,3})?(\\s?)([A-Z0-9]{2,3})($|\\s.*$)", "$1$2$3$4$5");
+				String region = zipRegion.replaceFirst(zip, "").trim();
+				
+				currentListing.setCountry(country);
+				if (!zip.equals(zipRegion)) {
+					currentListing.setZip(zip);
+					currentListing.setRegion(region);
+				}
 			}
 		}
 		
-		currentListing.setModelName(potentialBrand);
-		currentListing.setCategory(potentialCategory);
+		if (brand == null) {
+			brand = potentialBrand;
+		}
+		
+		if (category == null) {
+			category = potentialCategory;
+		}
+		
+		if (model == null) {
+			if (potentialModel != null) {
+				model = potentialModel.replaceFirst(brand, "").trim();
+			}
+		}
+		
+		currentListing.setModelName(model);
+		currentListing.setManName(brand);
+		currentListing.setCategory(category);
 		currentListing.setCatLang("DE");
+		currentListing.setDate(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
 	}
 
 	@Override
